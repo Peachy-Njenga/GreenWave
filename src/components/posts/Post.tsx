b28a1ccdc111477ba8afc576a2f1464656f000e9
Peachy-Njenga <peacheswagithi16@gmail.com>
2024-09-22 16:31:41 +0300

    feat: :art: added ky for http requests

--- src/components/posts/Post.tsx
+++ src/components/posts/Post.tsx
@@ -21,7 +21,7 @@ export default function Post({ post }: PostProps) {
                 </Link>
                 <Link href={`/posts/${post.id}`}
                     className="block text-sm text-muted-foreground hover:underline">
-                    {/* {formatRelativeDate(post.createdAt)} */}
+                    {formatRelativeDate(post.createdAt)}
                 </Link>
             </div>
         </div>
