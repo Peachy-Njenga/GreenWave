2e9b3a0a0cabcb640b76185850be4bc4e50f568b
Peachy-Njenga <peacheswagithi16@gmail.com>
2024-09-24 22:56:22 +0300

    feat: :sparkles: Added tabs for the for-you feed and following feed

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,13 +3,31 @@ import Post from "@/components/posts/Post";
 import PostEditor from "@/components/posts/editor/PostEditor";
 import prisma from "@/lib/prisma";
 import ForYouFeed from "./ForYouFeed";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import FollowingFeed from "./FollowingFeed";
 
 export default async function Home() {
   return (
     <main className="min-w-0 w-full gap-4 flex">
       <div className="w-full min-w-0 space-y-5">
       <PostEditor/>
-      <ForYouFeed/>
+      <Tabs>
+        <TabsList>
+          <TabsTrigger  value="for-you">
+            For you
+          </TabsTrigger>
+          <TabsTrigger value="following">
+            Following
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="for-you">
+    <ForYouFeed/>
+        </TabsContent>
+        <TabsContent value="following">
+          <FollowingFeed/>
+        </TabsContent>
+
+      </Tabs>
       </div>
       <TrendsSidebar />
       
